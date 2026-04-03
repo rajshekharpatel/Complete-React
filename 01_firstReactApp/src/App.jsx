6fe6ddb057862d61e533c880abc9ac9b4b58be9e
Raj Shekhar Patel <patelrajshekhar6@gmail.com>
2026-04-04 00:06:59 +0530

    Learning React

---
+++ 01_firstReactApp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hiii........</h1>
+      <p>Hello, I'm Raj Shekhar. The React Developer </p>
+    </>
+  )
+}
+
+export default App
